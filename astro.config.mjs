b7632044b14988f904471e56b4795327a656bca8
Carlos Ramos <57056947+CarlosIRamosV@users.noreply.github.com>
2025-04-19 21:56:47 -0600

    feat: add MDX support with @astrojs/mdx integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,13 @@
 import { defineConfig } from "astro/config";
 import alpinejs from "@astrojs/alpinejs";
 import tailwindcss from "@tailwindcss/vite";
-
 import sitemap from "@astrojs/sitemap";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://carlosiramosv.com",
-  integrations: [alpinejs(), sitemap()],
+  integrations: [alpinejs(), sitemap(), mdx()],
   vite: {
     plugins: [tailwindcss()],
   },
